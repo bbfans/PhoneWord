4bca13827aa53e3fa49292e3846b3a28e6624262
Jie Chen <jie_chen_2006@yahoo.ca>
2017-11-26 22:58:47 -0500

    Add project files.

---
+++ PhoneWord/MainActivity.cs
@@ -0,0 +1,52 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+using Android.Content;
+
+namespace PhoneWord
+{
+    [Activity(Label = "PhoneWord", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        static readonly List<string> phoneNumbers = new List<string>();
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+            TextView translatedPhoneWord = FindViewById<TextView>(Resource.Id.TranslatedPhoneWord);
+            Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+            Button translationHistoryButton = FindViewById<Button>(Resource.Id.TranslationHistoryButton);
+
+
+            translateButton.Click += (sender, e) =>
+            {
+                string translatedNumber = core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+                if (string.IsNullOrWhiteSpace(translatedNumber))
+                {
+                    translatedPhoneWord.Text = string.Empty;
+                }
+                else
+                {
+                    translatedPhoneWord.Text = translatedNumber;
+                    phoneNumbers.Add(translatedNumber);
+                    translationHistoryButton.Enabled = true;
+                }
+            };
+
+            translationHistoryButton.Click += (sender, e) =>
+            {
+                var intent = new Intent(this, typeof(TranslationHistoryActivity));
+                intent.PutStringArrayListExtra("phone_numbers", phoneNumbers);
+                StartActivity(intent);
+            };
+
+        }
+    }
+}
+
